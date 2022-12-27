1b6da8bd3e73c31896e12701756cde8e48414c9e
Yamanaka78 <jjbdumujpgm@gmail.com>
2022-12-27 23:54:20 +0900

    create account lock

--- app/Http/Controllers/Auth/AuthController.php
+++ app/Http/Controllers/Auth/AuthController.php
@@ -6,6 +6,8 @@ use App\Http\Controllers\Controller;
 use App\Http\Requests\LoginFormRequest;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Auth;
+use App\Models\User;
+
 class AuthController extends Controller
 {
     /**
@@ -23,12 +25,32 @@ class AuthController extends Controller
     {
         $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
-            $request->session()->regenerate();
+        $user = User::where('email', '=', $credentials['email'])->first();
+        if (!is_null($user)){
+            if ($user->locked_flg === 1){
+                return back()->withErrors([
+                    'login_error' => 'アカウントがロックされています。',
+                ]);
+            }
+            if (Auth::attempt($credentials)) {
+                $request->session()->regenerate();
+                if ($user->error_count !== 0) {
+                    $user->error_count = 0;
+                    $user->save();
+                }
+                return redirect()->route('home')->with('login_success','ログイン成功しました!');
+            }
+            $user->error_count = $user->error_count + 1;
+            if ($user->error_count > 5){
+                $user->locked_flg = 1;
+                $user->save();
+                return back()->withErrors([
+                    'login_error' => 'アカウントがロックされました。解除したい場合は運営に連絡してください',
+                ]);
+            }
+            $user->save();
 
-            return redirect()->route('home')->with('login_success','ログイン成功しました!');
-        };
-
+        }
         return back()->withErrors([
             'login_error' => 'メールアドレスかパスワードが間違っています。',
         ]);
